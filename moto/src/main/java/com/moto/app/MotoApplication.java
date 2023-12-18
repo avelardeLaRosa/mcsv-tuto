c2304f2e57346ed9d75ce3f60b7a73c86f68bc26
Alexander Velarde La Rosa <avelardelarosa@github.com>
2023-12-18 12:54:14 -0500

    eureka

--- moto/src/main/java/com/moto/app/MotoApplication.java
+++ moto/src/main/java/com/moto/app/MotoApplication.java
@@ -2,7 +2,9 @@ package com.moto.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MotoApplication {
 
