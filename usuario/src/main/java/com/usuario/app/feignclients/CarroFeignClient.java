f593b47b992f58b33969ca7bb8074388ce9f182c
Alexander Velarde La Rosa <avelardelarosa@github.com>
2024-01-11 18:58:39 -0500

    config server update

--- usuario/src/main/java/com/usuario/app/feignclients/CarroFeignClient.java
+++ usuario/src/main/java/com/usuario/app/feignclients/CarroFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "carro-service", url = "http://localhost:8081")
+@FeignClient(name = "carro-service")
 @RequestMapping("/carro")
 public interface CarroFeignClient {
 
--- usuario/src/main/java/com/usuario/app/feignclients/MotoFeignClient.java
+++ usuario/src/main/java/com/usuario/app/feignclients/MotoFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "moto-service", url = "http://localhost:8082")
+@FeignClient(name = "moto-service")
 @RequestMapping("/moto")
 public interface MotoFeignClient {
 
