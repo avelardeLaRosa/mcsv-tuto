866356ad006e1ea172212a6645d6c91ec013ae11
Alexander Velarde La Rosa <avelardelarosa@github.com>
2024-01-15 11:46:53 -0500

    v2

--- usuario/src/main/java/com/usuario/app/controller/UsuarioRESTController.java
+++ usuario/src/main/java/com/usuario/app/controller/UsuarioRESTController.java
@@ -4,7 +4,9 @@ import com.usuario.app.entities.UsuarioEntity;
 import com.usuario.app.models.CarroModel;
 import com.usuario.app.models.MotoModel;
 import com.usuario.app.service.IUsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +30,7 @@ public class UsuarioRESTController {
         return ResponseEntity.ok(service.getAll());
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackGetCarros")
     @GetMapping("/carro/{id}")
     public ResponseEntity<List<CarroModel>> getAllCarros(@PathVariable long id) {
         if (service.get(id) == null) {
@@ -36,6 +39,7 @@ public class UsuarioRESTController {
         return ResponseEntity.ok(service.getCarros(id));
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGetMotos")
     @GetMapping("/moto/{id}")
     public ResponseEntity<List<MotoModel>> getAllMotos(@PathVariable long id) {
         if (service.get(id) == null) {
@@ -57,11 +61,13 @@ public class UsuarioRESTController {
         return ResponseEntity.ok(service.add(u));
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackSaveCarro")
     @PostMapping("/carro/{id}")
     public ResponseEntity<CarroModel> addCarro(@PathVariable long id, @RequestBody CarroModel u) {
         return ResponseEntity.ok(service.saveCarro(id, u));
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackSaveMoto")
     @PostMapping("/moto/{id}")
     public ResponseEntity<MotoModel> addMoto(@PathVariable long id, @RequestBody MotoModel u) {
         return ResponseEntity.ok(service.saveMoto(id, u));
@@ -80,4 +86,37 @@ public class UsuarioRESTController {
         service.delete(id);
         return ResponseEntity.ok("Registro Eliminado");
     }
+
+
+    private ResponseEntity<List<CarroModel>> fallBackGetCarros(
+            @PathVariable("usuarioId") Integer id,
+            RuntimeException exception
+    ){
+        return new ResponseEntity("El usuario : " + id + " tiene los carros en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<CarroModel>> fallBackSaveCarro(
+            @PathVariable("usuarioId") Integer id,
+            @RequestBody CarroModel carroModel,
+            RuntimeException exception
+    ){
+        return new ResponseEntity("El usuario : " + id + " no tiene dinero para los carros", HttpStatus.OK);
+    }
+
+
+    private ResponseEntity<List<CarroModel>> fallBackGetMotos(
+            @PathVariable("usuarioId") Integer id,
+            RuntimeException exception
+    ){
+        return new ResponseEntity("El usuario : " + id + " tiene las motos en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<CarroModel>> fallBackSaveMoto(
+            @PathVariable("usuarioId") Integer id,
+            @RequestBody CarroModel carroModel,
+            RuntimeException exception
+    ){
+        return new ResponseEntity("El usuario : " + id + " no tiene dinero para las motos", HttpStatus.OK);
+    }
 }
+
